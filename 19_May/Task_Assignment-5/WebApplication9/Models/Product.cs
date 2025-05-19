4604ed71c5f1de571e718af4df0fb371986e73cd
b84user04 <dongre5998@gmail.com>
2025-05-19 17:27:42 +0530

    Migration

---
+++ 19_May/Task_Assignment-5/WebApplication9/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WebApplication9.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+                     
+    }
+    
+
+}
