10fecc82a9933850208ab6084bb390931328df62
b84user04 <dongre5998@gmail.com>
2025-05-20 12:28:02 +0530

    Model_Controller

---
+++ 20_May/Model_Controller_Crud_Operation/WebApplication11/Data/WebApplication11Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication11.Controllers;
+
+namespace WebApplication11.Data
+{
+    public class WebApplication11Context : DbContext
+    {
+        public WebApplication11Context (DbContextOptions<WebApplication11Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication11.Controllers.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ 20_May/Task_Assignment/WebApplication13/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication13.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public  int DeptNumber { get; set; }
+        public string DeptName { get; set; }
+    }
+}
