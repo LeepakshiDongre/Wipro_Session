e88e621b61343d8540519632c85a2339512869d4
b84user04 <dongre5998@gmail.com>
2025-06-03 11:56:49 +0530

    Assignment

---
+++ 3_Jun/Web_api_product_assignent/WebApplication20/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication20.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
